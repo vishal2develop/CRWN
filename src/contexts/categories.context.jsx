ab656fedff76f14c657fd62a09d77bd40a08f681
Vishal Borana <vishalborana@Vishals-MacBook-Air.local>
2022-08-06 19:52:54 +0530

    added Dynamic routing, fixed css

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useEffect, useState } from "react";
+import SHOP_DATA from "../shop-data.js";
+import {
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+} from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+  setProducts: () => null,
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    async function getCategories() {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoryMap);
+      console.log(categoryMap);
+    }
+    getCategories();
+  }, []);
+
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
